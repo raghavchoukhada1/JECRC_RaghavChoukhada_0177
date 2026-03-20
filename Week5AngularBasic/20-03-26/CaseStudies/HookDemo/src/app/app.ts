908877f10ec554a38ed9ccc8ca405cace6cff410
Raghav <raghavchoukhada@gmail.com>
2026-03-20 17:06:12 +0530

    Added

---
+++ Week5AngularBasic/20-03-26/CaseStudies/HookDemo/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Orderparent } from './orderparent/orderparent';
+
+@Component({
+  selector: 'app-root',
+  imports: [Orderparent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  
+}
