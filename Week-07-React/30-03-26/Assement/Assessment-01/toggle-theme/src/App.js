b148d7cfddb88d30634ad59bdb63d68140ba8159
Raghav <raghavchoukhada@gmail.com>
2026-03-30 11:20:05 +0530

    added

---
+++ Week-07-React/30-03-26/Assement/Assessment-01/toggle-theme/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ThemeToggle from './Components/ThemeToggle';
+
+function App() {
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
+}
+
+export default App;
